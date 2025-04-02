ec8f8ae0f7567f1b0d3cac55a22dedd390f57659
Machuge27 <mutaihillary3912@gmail.com>
2025-04-02 14:18:32 +0300

    Initial deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { getToken, removeToken } from '../utils/storage';
 
-// const API_BASE_URL = process.env.BASE_URL || 'https://mrsgrace.pythonanywhere.com/';
-const API_BASE_URL = 'http://192.168.100.6:8000/';
+const API_BASE_URL = process.env.BASE_URL || 'https://mrsgrace.pythonanywhere.com/';
+// const API_BASE_URL = 'http://192.168.100.6:8000/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
